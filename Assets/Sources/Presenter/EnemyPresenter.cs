43e008ef80e38c99f1469c81b51df7af5a3aaa82
Roman <103669215+Tikhonov-Roman@users.noreply.github.com>
2023-03-03 22:22:45 +0400

    init
    init

---
+++ Assets/Sources/Presenter/EnemyPresenter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EnemyPresenter : Presenter
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("Player") )
+        {
+            DestroyCompose();
+        }
+    }
+}
